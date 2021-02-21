a48e01418daa854e7dc55b3a94df428b5aeb96cc
nari2239 <oryolife@gmail.com>
2021-02-21 15:16:06 +0900

    新しい練習ページを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   root to: "tweets#index"
   get 'tweets/index'
-  resources :tweets
+  resources :tweets do
+    collection do
+      get 'practice'
+    end
+  end
 end
